993f21730dd711d92ebc5f48f6483eb6bd9f3b5f
Yael Chavez <you400@gmail.com>
2022-08-25 18:13:22 -0500

    fix route

---
+++ Basket.Api/Repositories/BasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Basket.Api.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.Api.Repositories;
+
+public class BasketRepository : IBasketRepository
+{
+    private readonly IDistributedCache distributedCache;
+
+    public BasketRepository(IDistributedCache distributedCache)
+    {
+        this.distributedCache = distributedCache;
+    }
+
+    public async Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart)
+    {
+        await distributedCache.SetStringAsync(shoppingCart.UserName,
+            JsonSerializer.Serialize(shoppingCart));
+
+        return await GetBasket(shoppingCart.UserName);
+    }
+
+    public async Task<ShoppingCart?> GetBasket(string userName)
+    {
+        var basket = await distributedCache.GetStringAsync(userName);
+
+        if (basket is null) return null;
+
+        return JsonSerializer.Deserialize<ShoppingCart>(basket);
+    }
+
+    public async Task DeleteBasket(string username)
+        => await distributedCache.RemoveAsync(username);
+}
+
---
+++ Basket.Api/Repositories/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.Api.Entities;
+
+namespace Basket.Api.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task DeleteBasket(string username);
+        Task<ShoppingCart?> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart);
+    }
+}
